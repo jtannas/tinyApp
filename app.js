8e5dc99414fb82034bd1823d2599217f6fc1269d
Joel Tannas <jtannas@gmail.com>
2018-01-23 15:47:24 -0700

    extend the app with body-parser for POST requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const DEFAULT_PORT = 8080;
