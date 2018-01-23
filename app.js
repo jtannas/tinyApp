a469ec3eb82fa63db21a806f327a99228a4bfa80
Joel Tannas <jtannas@gmail.com>
2018-01-23 14:12:39 -0700

    move the express_server to app.js

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-const http = require('http');
-const PORT = 8080;
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
 
-// a function which handles requests and sends response
-const requestHandler = function handlesIncomingRequests(request, response) {
-  if (request.url === "/") {
-    response.end("Welcome!");
-  } else if (request.url === "/urls") {
-    response.end("www.lighthouselabs.ca\nwww.google.com");
-  } else {
-    response.statusCode = 404;
-    response.end("Unknown Path");
-  }
+
+const DEFAULT_PORT = 8080;
+const PORT = process.env.PORT || DEFAULT_PORT;
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
 };
 
-const server = http.createServer(requestHandler);
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
 
-server.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
