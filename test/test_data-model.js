c7dd1152be6ca3a08ce4842c06c6d1dba678f4ff
Joel Tannas <jtannas@gmail.com>
2018-01-24 13:26:05 -0700

    test getRandomString

--- test/test_data-model.js
+++ test/test_data-model.js
@@ -7,7 +7,18 @@ const app = rewire('../data-model.js');
 const assert = chai.assert;
 
 describe('Random URL Char Generator', function() {
-  const getRandomChar = app.__get__('getRandomChar');
+  const getRandomChar = app.__get__('getRandomChar ');
+  const URL_SAFE_CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_~';
+
+  beforeEach(function(done) {
+    sinon.stub(Math, 'random').returns(0.99);
+    done();
+  });
+
+  afterEach(function(done) {
+    Math.random.restore();
+    done();
+  });
 
   it('should exist', function() {
     assert.exists(getRandomChar);
@@ -24,8 +35,45 @@ describe('Random URL Char Generator', function() {
   });
 
   it('should return a single character', function() {
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0.00; i < 1.00; i += 0.01) {
+      Math.random.restore();
+      sinon.stub(Math, 'random').returns(i);
       assert.lengthOf(getRandomChar(), 1);
     }
+  });
+
+  it('should return a URL safe character', function() {
+    for (let i = 0.00; i < 1.00; i += 0.01) {
+      Math.random.restore();
+      sinon.stub(Math, 'random').returns(i);
+      assert.include(URL_SAFE_CHARS, getRandomChar());
+    }
+  });
+});
+describe('Random URL Safe String Generator', function() {
+  const getRandomString = app.__get__('getRandomString');
+
+  it('should exist', function() {
+    assert.exists(getRandomString);
+  });
+
+  it('should be a function', function() {
+    assert.isFunction(getRandomString);
+  });
+
+  it('should return a 6 character string when given no arguments', function() {
+    assert.lengthOf(getRandomString(), 6);
+  });
+
+  it('should return zero characters when given a zero input', function() {
+    assert.lengthOf(getRandomString(0), 0);
+  });
+
+  it('should return zero characters when given a negative input', function() {
+    assert.lengthOf(getRandomString(-1), 0);
+  });
+
+  it('should return 99 characters when given a 99 input', function() {
+    assert.lengthOf(getRandomString(99), 99);
   });
 });
